62d1ee9558f611547a42716e5b1e75ce046f1c01
meShailu <123243572+meShailu@users.noreply.github.com>
2023-09-03 17:47:36 +0200

    Updating Event schema#

--- db/Models/Event.js
+++ db/Models/Event.js
@@ -3,9 +3,15 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const eventSchema = new Schema({
+  type: { type: String, required: true },
   title: { type: String, required: true },
-  datetime: { type: String, required: true },
-  location: { type: String, required: true },
+  start_at: { type: String, required: true },
+  ends_at: { type: String, required: true },
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  country: { type: String, required: true },
+  hosted_by: { type: String, required: true },
+  tags: { type: Array, required: true },
   mapURL: { type: String, required: true },
   description: { type: String, required: true },
 });
